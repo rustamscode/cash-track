05247130e9ca2ea1114f551252d7e48eabe67163
Agagyl <agagylrustamov@gmail.com>
2025-04-07 21:17:29 +0400

    feat-1 | Repos for transaction and user create

---
+++ src/main/java/cash_track/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package cash_track.repository;
+
+import cash_track.persistance.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+}
