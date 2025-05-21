84f4a0fdb1896dab51f712729b80502f7cc1cd09
Rustamscode <agagylrustamov@gmail.com>
2025-05-22 01:08:16 +0400

    feat-5(transaction): add POST endpoint for transaction search

--- src/main/java/cash_track/controller/impl/TransactionControllerImpl.java
+++ src/main/java/cash_track/controller/impl/TransactionControllerImpl.java
@@ -2,20 +2,26 @@ package cash_track.controller.impl;
 
 import cash_track.controller.TransactionController;
 import cash_track.dto.request.TransactionCreateRq;
+import cash_track.dto.request.TransactionSearchRq;
 import cash_track.dto.request.TransactionUpdateRq;
 import cash_track.dto.response.ResponseDto;
 import cash_track.dto.response.TransactionRs;
 import cash_track.service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.UUID;
 
-import static cash_track.util.ResponseMessageUtil.TRANSACTIONS_FETCHED;
-import static cash_track.util.ResponseMessageUtil.TRANSACTION_CREATED;
-import static cash_track.util.ResponseMessageUtil.TRANSACTION_DELETED;
-import static cash_track.util.ResponseMessageUtil.TRANSACTION_UPDATED;
+import static cash_track.util.message.ExceptionMessageUtil.AUTHENTICATION_CREDENTIALS_NOT_FOUND;
+import static cash_track.util.message.ResponseMessageUtil.TRANSACTIONS_FETCHED;
+import static cash_track.util.message.ResponseMessageUtil.TRANSACTION_CREATED;
+import static cash_track.util.message.ResponseMessageUtil.TRANSACTION_DELETED;
+import static cash_track.util.message.ResponseMessageUtil.TRANSACTION_UPDATED;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,18 +30,23 @@ public class TransactionControllerImpl implements TransactionController {
   private final TransactionService transactionService;
 
   @Override
-  public ResponseDto<UUID> createTransaction(TransactionCreateRq transactionCreateRq) {
+  public ResponseDto<UUID> createTransaction(TransactionCreateRq request) {
     return ResponseDto.<UUID>builder()
         .message(TRANSACTION_CREATED)
-        .data(transactionService.createUserTransaction(transactionCreateRq))
+        .data(transactionService.createUserTransaction(request))
         .build();
   }
 
   @Override
-  public ResponseDto<List<TransactionRs>> getUserTransactions() {
-    return ResponseDto.<List<TransactionRs>>builder()
+  public ResponseDto<Page<TransactionRs>> getTransactions(UserDetails userDetails, TransactionSearchRq request, Pageable pageable) {
+    String username = userDetails.getUsername();
+    if (StringUtils.isBlank(username)) {
+      throw new AuthenticationCredentialsNotFoundException(AUTHENTICATION_CREDENTIALS_NOT_FOUND);
+    }
+
+    return ResponseDto.<Page<TransactionRs>>builder()
         .message(TRANSACTIONS_FETCHED)
-        .data(transactionService.getUserTransactions())
+        .data(transactionService.getTransactions(username, request, pageable))
         .build();
   }
 
